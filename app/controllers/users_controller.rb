8ceefb665e457c796be5dc4fef906c9d578a39ef
David Pitz <80120174+D-pitz@users.noreply.github.com>
2021-06-21 18:40:03 -0500

    Added User update functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,22 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        @user = User.find(current_user.id)
+        @user.update(user_update)
+        if @user.valid?
+            @user.save
+            flash[:success] = "#{current_user.name} succesfully updated"
+        redirect_to "/users/#{current_user.id}"
+        else 
+            flash[:errors] = @user.errors.full_messages
+            redirect_to :back
+        end
+    end
+
     def logout
         reset_session
         redirect_to "/users"
@@ -22,5 +38,8 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    def user_update
+        params.require(:user).permit(:name, :email)
     end
 end
