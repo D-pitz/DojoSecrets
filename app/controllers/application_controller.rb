0353dbd9b2d8d20c8667e62615f8286951da6b6a
David Pitz <80120174+D-pitz@users.noreply.github.com>
2021-06-22 15:56:39 -0500

    Added controller filter, only logged in user can get past login page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,10 @@ class ApplicationController < ActionController::Base
     User.find(session[:user]) if session[:user]
   end
   helper_method :current_user
+
+  def require_login
+    flash.alert = "You must be logged in to view this page."
+    redirect_to new_session_path unless current_user
+  end
 end
 
